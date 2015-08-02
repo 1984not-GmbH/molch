67758abd51ff95a915ef8516e52fa632925bc5be
Max Bruckner <max@maxbruckner.de>
2015-08-02 11:27:30 +0200

    message: Extract header extraction to separate function.

--- lib/message.c
+++ lib/message.c
@@ -101,6 +101,43 @@ int encrypt_message(
 }
 
 /*
+ * Extract the header, nonce and mac from a packet without verifying it's
+ * integrity.
+ *
+ * This is only used internally.
+ */
+int extract_header_nonce_and_mac_without_verifying(
+		unsigned char * const header,
+		size_t * const header_length,
+		unsigned char * const nonce, //crypto_secretbox_NONCEBYTES
+		unsigned char * const mac, //crypto_onetimeauth_BYTES
+		const unsigned char * const packet,
+		const size_t packet_length) {
+	//first byte of the header is it's length
+	const size_t HEADER_LENGTH = packet[0];
+
+	const size_t PRE_CIPHERTEXT_LENGTH = 1 + HEADER_LENGTH + crypto_secretbox_NONCEBYTES + crypto_onetimeauth_BYTES;
+
+	if ((PRE_CIPHERTEXT_LENGTH + crypto_secretbox_MACBYTES + 255) > packet_length) {
+		//packet isn't long enough (header field probably has incorrect value)
+		return -10;
+	}
+
+	//copy the header
+	memcpy(header, packet + 1, HEADER_LENGTH);
+
+	//copy the nonce
+	memcpy(nonce, packet + 1 + HEADER_LENGTH, crypto_secretbox_NONCEBYTES);
+
+	//copy the MAC
+	memcpy(mac, packet + 1 + HEADER_LENGTH + crypto_secretbox_NONCEBYTES, crypto_onetimeauth_BYTES);
+
+	*header_length = HEADER_LENGTH;
+
+	return 0;
+}
+
+/*
  * Decrypt a message with a symmetric key and verify the headers integrity.
  *
  * The message and header buffers should be as long as the packet buffer.
@@ -113,29 +150,42 @@ int decrypt_message(
 		const unsigned char * const packet,
 		const size_t packet_length,
 		const unsigned char * const key) { //crypto_secretbox_KEYBYTES
-	//first byte of the packet is the header length
-	const size_t HEADER_LENGTH = packet[0];
+	//buffers for the header components
+	unsigned char * const header_buffer = malloc(packet_length);
+	unsigned char nonce[crypto_secretbox_NONCEBYTES];
+	unsigned char mac[crypto_onetimeauth_BYTES];
 
-	const size_t PRE_CIPHERTEXT_LENGTH = 1 + HEADER_LENGTH + crypto_secretbox_NONCEBYTES + crypto_onetimeauth_BYTES;
-
-	if ((PRE_CIPHERTEXT_LENGTH + crypto_secretbox_MACBYTES + 255) > packet_length) {
-		//packet isn't long enough (header field has incorrect value [probably])
-		return -10;
+	int status;
+	status = extract_header_nonce_and_mac_without_verifying(
+			header,
+			header_length,
+			nonce,
+			mac,
+			packet,
+			packet_length);
+	if (status != 0) {
+		*header_length = 0;
+		free(header_buffer);
+		return status;
 	}
 
+	const size_t PRE_CIPHERTEXT_LENGTH = 1 + *header_length + crypto_secretbox_NONCEBYTES + crypto_onetimeauth_BYTES;
+
 	//authenticate header and nonce
-	int status;
 	status = crypto_onetimeauth_verify(
-			packet + HEADER_LENGTH + 1 + crypto_secretbox_NONCEBYTES, //MAC
+			mac, //MAC
 			packet, //input
-			1 + HEADER_LENGTH + crypto_secretbox_NONCEBYTES, //input_length
+			1 + *header_length + crypto_secretbox_NONCEBYTES, //input_length
 			key);
 	if (status != 0) {
+		*header_length = 0;
+		free(header_buffer);
 		return status;
 	}
 
 	//copy header
-	memcpy(header, packet + 1, HEADER_LENGTH);
+	memcpy(header, header_buffer, *header_length);
+	free(header_buffer);
 
 	const size_t CIPHERTEXT_LENGTH = packet_length - PRE_CIPHERTEXT_LENGTH;
 
@@ -148,7 +198,7 @@ int decrypt_message(
 			plaintext_buffer, //output
 			packet + PRE_CIPHERTEXT_LENGTH,
 			CIPHERTEXT_LENGTH,
-			packet + 1 + HEADER_LENGTH, //nonce
+			packet + 1 + *header_length, //nonce
 			key);
 	if (status != 0) {
 		sodium_memzero(plaintext_buffer, PLAINTEXT_LENGTH);
@@ -173,9 +223,6 @@ int decrypt_message(
 	memcpy(message, plaintext_buffer, *message_length);
 	sodium_memzero(plaintext_buffer, PLAINTEXT_LENGTH);
 	free(plaintext_buffer);
-
-	//copy header length to output
-	*header_length = HEADER_LENGTH;
 
 	return 0;
 }
