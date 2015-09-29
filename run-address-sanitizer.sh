fe9fd7a3c7cf34a7550ccfd95dc529457ad6d994
Max Bruckner <max@maxbruckner.de>
2015-09-29 10:49:00 +0200

    address-sanitizer: disable valgrind

--- run-address-sanitizer.sh
+++ run-address-sanitizer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 [[ ! -e address-sanitizer ]] && mkdir address-sanitizer
 cd address-sanitizer
-cmake .. -DCMAKE_C_FLAGS='-fsanitize=address'
+cmake .. -DCMAKE_C_FLAGS='-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
 make clean
 make
 make test
