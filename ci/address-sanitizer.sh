205bd1e15af5bb9e0292b81bfe50fa501f6b5839
Max Bruckner <max@maxbruckner.de>
2015-09-29 23:48:00 +0200

    address-sanitizer: also check for use after return

--- ci/address-sanitizer.sh
+++ ci/address-sanitizer.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 [ ! -e address-sanitizer ] && mkdir address-sanitizer
 cd address-sanitizer
-cmake .. -DCMAKE_C_FLAGS='-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
+cmake .. -DCMAKE_C_FLAGS='-fsanitize=address -O1 -fno-omit-frame-pointer -fno-common -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
 make clean
 make
+export ASAN_OPTIONS="$ASAN_OPTIONS:detect_stack_use_after_return=1"
 make test
