c4181d35fa3185e4b15d8a19b68dd9c1d8a37fed
Max Bruckner <max@maxbruckner.de>
2015-10-03 16:39:57 +0200

    cmake/FindSodium: Use lower case cmake commands throughout

--- cmake/FindSodium.cmake
+++ cmake/FindSodium.cmake
@@ -7,12 +7,12 @@
 #  SODIUM_FOUND          - True if libsodium found.
 #
 
-FIND_LIBRARY(SODIUM_LIBRARY NAMES sodium libsodium HINTS ${SODIUM_ROOT_DIR}/lib)
+find_library(SODIUM_LIBRARY NAMES sodium libsodium HINTS ${SODIUM_ROOT_DIR}/lib)
 find_path(SODIUM_INCLUDE_DIR NAMES sodium.h HINTS ${SODIUM_ROOT_DIR}/include)
 
 # handle the QUIETLY and REQUIRED arguments and set SODIUM_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Sodium REQUIRED_VARS SODIUM_LIBRARY SODIUM_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(SODIUM_LIBRARY SODIUM_INCLUDE_DIR)
+mark_as_advanced(SODIUM_LIBRARY SODIUM_INCLUDE_DIR)
