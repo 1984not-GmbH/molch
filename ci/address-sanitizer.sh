067866a1002c7a8e54dc21b5db9b55ba307fa1b0
Max Bruckner <max@maxbruckner.de>
2015-12-18 13:46:31 +0100

    ci: use clang for address sanitizer and properly check if it is available

--- ci/address-sanitizer.sh
+++ ci/address-sanitizer.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-if [ $(uname) == "Darwin" ]; then
-    #Skip test on Mac OS X
-    #TODO Make this work!
-    exit 0
-fi
 [ ! -e address-sanitizer ] && mkdir address-sanitizer
 cd address-sanitizer
-cmake .. -DCMAKE_C_FLAGS='-fsanitize=address -O1 -fno-omit-frame-pointer -fno-common -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
+#check if address sanitizer is available
+echo "int main(void) {return 0;}" > test.c
+if ! clang -fsanitize=address test.c -o /dev/null > /dev/null; then
+    echo AddressSanitizer not available. Skipping ...
+    rm test.c
+    exit 0
+fi
+rm test.c
+
+export CC=clang
+cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS='-fsanitize=address -O1 -fno-omit-frame-pointer -fno-common -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
 make clean
 make
-export ASAN_OPTIONS="$ASAN_OPTIONS:detect_stack_use_after_return=1"
+export ASAN_OPTIONS="$ASAN_OPTIONS:detect_stack_use_after_return=1:check_initialization_order=1"
 make test
