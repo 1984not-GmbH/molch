17615e7a84b86b55d502dbaf6640260c6a0f4178
Max Bruckner <max@maxbruckner.de>
2016-07-03 23:57:11 +0200

    Change license from LGPL2.1 to ISC

--- bindings/base64.lua
+++ bindings/base64.lua
@@ -1,38 +1,58 @@
 -- Base64-encoding
 -- Sourced from http://en.wikipedia.org/wiki/Base64
 --
--- Copyright (c) 2012, Daniel Lindsley
--- All rights reserved.
---
--- Modified by: Max Bruckner (FSMaxB)
+-- Changes to the original:
 -- * remove unnecessary "require"
 -- * remove __ attributes
 -- * rename "to_base64" -> "encode" and "from_base64" -> "decode"
 -- * make it a loadable module via require
 -- * fix use of the modulo operator (math.mod -> %)
 --
--- Redistribution and use in source and binary forms, with or without
--- modification, are permitted provided that the following conditions are met:
+-- ISC License
 --
--- * Redistributions of source code must retain the above copyright notice, this
---   list of conditions and the following disclaimer.
--- * Redistributions in binary form must reproduce the above copyright notice,
---   this list of conditions and the following disclaimer in the documentation
---   and/or other materials provided with the distribution.
--- * Neither the name of the base64 nor the names of its contributors may be
---   used to endorse or promote products derived from this software without
---   specific prior written permission.
+-- Copyright (C) 2015-2016 1984not Security GmbH
+-- Author: Max Bruckner (FSMaxB)
 --
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
--- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
--- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
--- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
--- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
--- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
--- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
--- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
--- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
--- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+--
+-- This file incorporates work covered by the following license notice:
+--
+-- | Copyright (c) 2012, Daniel Lindsley
+-- | All rights reserved.
+-- |
+-- | Redistribution and use in source and binary forms, with or without
+-- | modification, are permitted provided that the following conditions are met:
+-- |
+-- | * Redistributions of source code must retain the above copyright notice, this
+-- |   list of conditions and the following disclaimer.
+-- | * Redistributions in binary form must reproduce the above copyright notice,
+-- |   this list of conditions and the following disclaimer in the documentation
+-- |   and/or other materials provided with the distribution.
+-- | * Neither the name of the base64 nor the names of its contributors may be
+-- |   used to endorse or promote products derived from this software without
+-- |   specific prior written permission.
+-- |
+-- | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- | ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- | WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- | DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- | SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- | OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 local base64 = {}
 
