06a3bc0e134c2edbb44c84d2641456400676f1ca
Max Bruckner <max@maxbruckner.de>
2017-09-10 03:23:16 +0200

    Android: Update creation of crossfiles

--- android/create-crossfiles.sh
+++ android/create-crossfiles.sh
@@ -10,12 +10,14 @@ c = '${PWD}/${architecture}/bin/clang'
 cpp = '${PWD}/${architecture}/bin/workaround-clang++'
 strip = '${PWD}/${architecture}/bin/arm-linux-androideabi-strip'
 ar = '${PWD}/${architecture}/bin/arm-linux-androideabi-ar'
+ld = '${PWD}/${architecture}/bin/arm-linux-androideabi-ld.gold'
+pkgconfig = '/usr/bin/false'
 
 [properties]
 c_link_args = ['-pie']
 cpp_link_args = ['-static-libstdc++', '-pie']
-c_args = ${flags}
-cpp_args = ${flags}
+c_args = [${flags}]
+cpp_args = [${flags}]
 lua_bindings=false
 
 [host_machine]
@@ -28,13 +30,13 @@ endian = 'little'"
 }
 
 function make_files() {
-	make_cross_file "arm" "armv6" "['-mthumb', '-marm', '-march=armv6']"
-	make_cross_file "arm" "armv7-a" "['-mfloat-abi=softfp', '-mfpu=vfpv3-d16', '-mthumb', '-marm', '-march=armv7-a']"
-	make_cross_file "arm64" "armv8-a" "['-march=armv8-a']"
-	make_cross_file "mips32" "mips32" "[]"
-	make_cross_file "mips64" "mips64r6" "['-march=mips64r6']"
-	make_cross_file "x86" "i686" "['-march=i686']"
-	make_cross_file "x86_64" "westmere" "['-march='westmere']"
+	make_cross_file "arm" "armv6" "'-Os', '-fPIC', '-mthumb', '-marm', '-march=armv6'"
+	make_cross_file "arm" "armv7-a" "'-Os', '-fPIC', -mfloat-abi=softfp', '-mfpu=vfpv3-d16', '-mthumb', '-marm', '-march=armv7-a'"
+	make_cross_file "arm64" "armv8-a" "'-Os', '-fPIC', '-march=armv8-a'"
+	make_cross_file "mips32" "mips32" "'-Os', '-fPIC'"
+	make_cross_file "mips64" "mips64r6" "'-Os', '-fPIC', '-march=mips64r6'"
+	make_cross_file "x86" "i686" "'-Os', '-fPIC', '-march=i686'"
+	make_cross_file "x86_64" "westmere" "'-Os', '-fPIC', '-march=westmere'"
 }
 
 function delete_files() {
