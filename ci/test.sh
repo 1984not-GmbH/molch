92077c6c7368afaeeaa24f6da4685adb9b61ea5c
Max Bruckner <max@maxbruckner.de>
2017-10-01 21:19:17 +0200

    CI: Disable valgrind if it doesn't work

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+function valgrind_works {
+    if ! hash valgrind 2> /dev/null; then
+        return 1
+    fi
+
+
+    local output_name=valgrind_works-test
+    echo -e "#include <stdio.h>\n int main() {puts(\"Hello World!\");}" > "$output_name".c
+    gcc "$output_name".c -o "$output_name"
+
+    valgrind --suppressions=../valgrind.supp ./"$output_name" #2> /dev/null
+    local result=$?
+    rm "$output_name"{,.c}
+    return $result
+}
+
 output_dir=build
 [ -e build ] && rm -r "$output_dir"
 mkdir "$output_dir"
@@ -16,12 +32,13 @@ else
     exit $?
 fi
 
-if hash valgrind 2> /dev/null; then
+if valgrind_works; then
     if meson test --setup valgrind; then
         true
     else
         exit $?
     fi
 else
+    echo "Valgrind doesn't work!"
     true
 fi
