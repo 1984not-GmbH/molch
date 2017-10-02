62437aa3da35e1616c516f47794b6806c93b2c21
Max Bruckner <max@maxbruckner.de>
2017-10-02 20:34:15 +0200

    Control-Tests for ASan and UbSan
    Add tests that should fail with ASan and UbSan

---
+++ test/asan-test.cpp
@@ -0,0 +1,33 @@
+/*
+ * Molch, an implementation of the axolotl ratchet based on libsodium
+ *
+ * ISC License
+ *
+ * Copyright (C) 2015-2016 1984not Security GmbH
+ * Author: Max Bruckner (FSMaxB)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <iostream>
+#include <limits>
+
+int main(void) noexcept {
+	int array[10]{0, 1, 2, 3, 4, 5, 6, 7, 8 , 9};
+	for (size_t i{0}; i <= 10; i++) {
+		std::cout << array[i] << ", ";
+	}
+	std::cout << std::endl;
+
+	return EXIT_SUCCESS;
+}
