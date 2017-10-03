d8d05ba47ecfada86db3e12c65cc6ffb5c8f96c1
Max Bruckner <max@maxbruckner.de>
2017-10-03 23:39:37 +0200

    Android: Enable Link Time Optimization

--- android/build.sh
+++ android/build.sh
@@ -5,5 +5,5 @@ builddir="${cpu}-build"
 
 mkdir -p "$builddir"
 cd "$builddir" || exit 1
-meson ../.. --cross-file "../${cpu}-cross.txt" -Dlua_bindings=false
+meson ../.. --cross-file "../${cpu}-cross.txt" --buildtype release -Db_lto=true -Dlua_bindings=false
 ninja
