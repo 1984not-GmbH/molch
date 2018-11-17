02bc60438ad09c1d939038c4ae6c15a1a89fd22b
Max Bruckner <max@maxbruckner.de>
2018-11-17 18:12:44 +0100

    JNI: Add .editorconfig

--- bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
+++ bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
@@ -1,15 +1,14 @@
 package de.nineteen.eighty.four.not.molch;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MolchTest {
-    @Test
-    @DisplayName("Test that JUnit works")
-    void testThatJUnitWorks() {
-        assertTrue(true);
-    }
+	@Test
+	@DisplayName("Test that JUnit works")
+	void testThatJUnitWorks() {
+		assertTrue(true);
+	}
 }
