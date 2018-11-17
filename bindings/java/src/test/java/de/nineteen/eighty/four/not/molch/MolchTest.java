beb47abb3c612df9a960d588267cbf3a45339c61
Max Bruckner <max@maxbruckner.de>
2018-11-17 17:23:38 +0100

    Add the gradle build system

---
+++ bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
@@ -0,0 +1,15 @@
+package de.nineteen.eighty.four.not.molch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MolchTest {
+    @Test
+    @DisplayName("Test that JUnit works")
+    void testThatJUnitWorks() {
+        assertTrue(true);
+    }
+}
