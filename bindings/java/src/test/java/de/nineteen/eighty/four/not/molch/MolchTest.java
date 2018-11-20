f72b58abd6bd98bd9fa1b496c70a4c4b5723dce2
Max Bruckner <max@maxbruckner.de>
2018-11-20 22:05:31 +0100

    JNI: Implement getUserIdSize()

--- bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
+++ bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
@@ -3,6 +3,7 @@ package de.nineteen.eighty.four.not.molch;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MolchTest {
@@ -10,5 +11,11 @@ class MolchTest {
 	@DisplayName("Test that JUnit works")
 	void testThatJUnitWorks() {
 		assertTrue(true);
+	}
+
+	@Test
+	@DisplayName("Test getUserIdSize")
+	void testGetUserIdSize() {
+		assertEquals(32, Molch.getUserIdSize() );
 	}
 }
