30322f462eec5f23aa1d738e2102b267d4169672
Max Bruckner <max@maxbruckner.de>
2018-11-20 21:07:42 +0100

    JNI: Create java class

---
+++ bindings/java/src/main/java/de/nineteen/eighty/four/not/molch/Molch.java
@@ -0,0 +1,88 @@
+package de.nineteen.eighty.four.not.molch;
+
+import java.util.Optional;
+
+enum MessageType {
+	PREKEY,
+	NORMAL,
+	INVALID,
+}
+
+
+class Molch {
+	static {
+		System.loadLibrary("molch-jni");
+	}
+
+	class CreateUserResult {
+		public byte[] userId;
+		public byte[] prekeyList;
+		public byte[] backupKey;
+		public Optional<byte[]> backup;
+	}
+
+	static native CreateUserResult createUser(boolean createBackup, Optional<byte[]> randomSpice) throws Exception;
+	static native Optional<byte[]> destroyUser(byte[] id, boolean createBackup) throws Exception;
+	static native long countUsers();
+	static native void destroyAllUsers();
+	static native byte[][] listUsers();
+
+	static native MessageType getMessageType(byte[] packet);
+
+	class SendConversationResult {
+		public byte[] conversationId;
+		public byte[] packet;
+		public Optional<byte[]> backup;
+	}
+
+	static native SendConversationResult startSendConversation(
+			byte[] senderId,
+			byte[] receiverId,
+			byte[] prekeyList,
+			byte[] message,
+			boolean createBackup) throws Exception;
+
+	class ReceiveConversationResult {
+		public byte[] conversationId;
+		public byte[] prekeyList;
+		public byte[] message;
+		public Optional<byte[]> backup;
+	}
+
+	static native ReceiveConversationResult startReceiveConversation(
+			byte[] receiverId,
+			byte[] senderId,
+			byte[] packet,
+			boolean createBackup) throws Exception;
+
+	class EncryptResult {
+		public byte[] packet;
+		public Optional<byte[]> conversationBackup;
+	}
+
+	static native EncryptResult encrypt(
+			byte[] conversationId,
+			byte[] message,
+			boolean createConversationBackup) throws Exception;
+
+	class DecryptResult {
+		public long messageNumber;
+		public long previousMessageNumber;
+		public byte[] message;
+		public Optional<byte[]> conversationBackup;
+	}
+
+	static native DecryptResult decrypt(
+			byte[] conversationId,
+			byte[] packet,
+			boolean createConversationBackup) throws Exception;
+
+	static native Optional<byte[]> endConversation(byte[] conversationId, boolean createBackup) throws Exception;
+	static native byte[][] listConversations(byte[] userId) throws Exception;
+	static native byte[] exportConversation(byte[] conversationId) throws Exception;
+	static native byte[] importConversation(byte[] conversationBackup, byte[] backupKey) throws Exception;
+	static native byte[] exportUsers() throws Exception;
+	static native byte[] importUsers(byte[] backup, byte[] backupKey) throws Exception;
+	static native byte[] getPrekeyList(byte[] userId) throws Exception;
+	static native byte[] updateBackupKey();
+}
---
+++ bindings/java/src/main/java/de/nineteen/eighty/four/not/molch/MolchException.java
@@ -0,0 +1,7 @@
+package de.nineteen.eighty.four.not.molch;
+
+public class MolchException extends Exception {
+	private boolean isOk;
+	public String status;
+	public String message;
+}
