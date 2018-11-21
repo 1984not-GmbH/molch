0b5f2e5ca3d8a233bf3027f3212dbb20bb5eddff
Max Bruckner <max@maxbruckner.de>
2018-11-21 20:14:59 +0100

    Java: Use hamcrest for assertions

--- bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
+++ bindings/java/src/test/java/de/nineteen/eighty/four/not/molch/MolchTest.java
@@ -3,31 +3,31 @@ package de.nineteen.eighty.four.not.molch;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.*;
 
 class MolchTest {
 	@Test
 	@DisplayName("Test that JUnit works")
 	void testThatJUnitWorks() {
-		assertTrue(true);
+	    assertThat(true, is(true));
 	}
 
 	@Test
 	@DisplayName("Test getUserIdSize")
 	void testGetUserIdSize() {
-		assertEquals(32, Molch.getUserIdSize() );
+		assertThat(Molch.getUserIdSize(), is(32L));
 	}
 
 	@Test
 	@DisplayName("Test getConversationIdSize")
 	void testGetConversationIdSize() {
-		assertEquals(32, Molch.getConversationIdSize());
+	    assertThat(Molch.getConversationIdSize(), is(32L));
 	}
 
 	@Test
 	@DisplayName("Test getBackupKeySize")
 	void testGetBackupKeySize() {
-		assertEquals(32, Molch.getBackupKeySize());
+		assertThat(Molch.getBackupKeySize(), is(32L));
 	}
 }
