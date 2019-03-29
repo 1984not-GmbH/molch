b331789e5d3d202b267393bd217e8d63b64836a9
Max Bruckner <max@maxbruckner.de>
2019-03-29 10:54:22 +0100

    JNI: Let unimplemented methods throw

--- bindings/java/src/main/java/de/nineteen/eighty/four/not/molch/Molch.java
+++ bindings/java/src/main/java/de/nineteen/eighty/four/not/molch/Molch.java
@@ -24,12 +24,18 @@ abstract class Molch {
 	}
 
 	static native CreateUserResult createUser(byte[] randomSpice /* optional */) throws Exception;
-	static native byte[] destroyUser(byte[] id, boolean createBackup) throws Exception; /* optionaly returns backup */
+	static byte[] destroyUser(byte[] id, boolean createBackup) throws Exception { /* optionaly returns backup */
+		throw notImplemented();
+	}
 	static native long countUsers();
 	static native void destroyAllUsers();
-	static native byte[][] listUsers() throws Exception;
+	static byte[][] listUsers() throws Exception {
+		throw notImplemented();
+	}
 
-	static native MessageType getMessageType(byte[] packet);
+	static MessageType getMessageType(byte[] packet) {
+		throw notImplemented();
+	}
 
 	static class SendConversationResult {
 		public byte[] conversationId;
@@ -37,12 +43,14 @@ abstract class Molch {
 		public byte[] backup;
 	}
 
-	static native SendConversationResult startSendConversation(
+	static SendConversationResult startSendConversation(
 			byte[] senderId,
 			byte[] receiverId,
 			byte[] prekeyList,
 			byte[] message,
-			boolean createBackup) throws Exception;
+			boolean createBackup) throws Exception {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 
 	static class ReceiveConversationResult {
 		public byte[] conversationId;
@@ -51,11 +59,13 @@ abstract class Molch {
 		public byte[] backup;
 	}
 
-	static native ReceiveConversationResult startReceiveConversation(
+	static ReceiveConversationResult startReceiveConversation(
 			byte[] receiverId,
 			byte[] senderId,
 			byte[] packet,
-			boolean createBackup) throws Exception;
+			boolean createBackup) throws Exception {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 
 	static class EncryptResult {
 		public byte[] packet;
@@ -74,17 +84,39 @@ abstract class Molch {
 		public byte[] conversationBackup;
 	}
 
-	static native DecryptResult decrypt(
+	static DecryptResult decrypt(
 			byte[] conversationId,
 			byte[] packet,
-			boolean createConversationBackup) throws Exception;
+			boolean createConversationBackup) throws Exception {
+		throw notImplemented();
+	}
 
-	static native byte[] endConversation(byte[] conversationId, boolean createBackup) throws Exception;
-	static native byte[][] listConversations(byte[] userId) throws Exception;
-	static native byte[] exportConversation(byte[] conversationId) throws Exception;
-	static native byte[] importConversation(byte[] conversationBackup, byte[] backupKey) throws Exception;
-	static native byte[] exportUsers() throws Exception;
-	static native byte[] importUsers(byte[] backup, byte[] backupKey) throws Exception;
-	static native byte[] getPrekeyList(byte[] userId) throws Exception;
-	static native byte[] updateBackupKey();
+	static byte[] endConversation(byte[] conversationId, boolean createBackup) throws Exception {
+		throw notImplemented();
+	}
+	static byte[][] listConversations(byte[] userId) throws Exception {
+		throw notImplemented();
+	}
+	static byte[] exportConversation(byte[] conversationId) throws Exception {
+		throw notImplemented();
+	}
+	static byte[] importConversation(byte[] conversationBackup, byte[] backupKey) throws Exception {
+		throw notImplemented();
+	}
+	static byte[] exportUsers() throws Exception {
+		throw notImplemented();
+	}
+	static byte[] importUsers(byte[] backup, byte[] backupKey) throws Exception {
+		throw notImplemented();
+	}
+	static byte[] getPrekeyList(byte[] userId) throws Exception {
+		throw notImplemented();
+	}
+	static byte[] updateBackupKey() {
+		throw notImplemented();
+	}
+
+	private static UnsupportedOperationException notImplemented() {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 }
